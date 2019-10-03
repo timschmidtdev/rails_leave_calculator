29ce1e5b088fd904dcec3657bf20b358de44f5aa
timschmidtdev <schmidt.tim.j@gmail.com>
2019-10-03 01:57:51 -0400

    plan create actions

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,9 +4,27 @@ class PlansController < ApplicationController
   end
 
   def show
+    @plan = Plan.find(params[:id])
   end
 
   def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new
+    @plan.plan_type = params[:plan][:plan_type]
+    @plan.start = params[:plan][:start]
+    @plan.length = params[:plan][:length]
+    @plan.unit = params[:plan][:unit]
+
+    if @plan.save
+      flash[:notice] = "Plan was saved."
+      redirect_to @plan
+    else
+      flash.now[:alert] = "There was an error saving the plan. Please try again."
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/plans_controller_spec.rb
+++ spec/controllers/plans_controller_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PlansController, type: :controller do
-  let(:my_plan) { Plan.create!(plan_type: RandomData.random_sentence, start: RandomData.random_date, length: RandomData.random_decimal, unit: RandomData.random_word) }
+  let(:my_plan) { Plan.create!(plan_type: RandomData.random_sentence,
+                              start: RandomData.random_date,
+                              length: RandomData.random_decimal,
+                              unit: RandomData.random_word) }
 
   describe "GET #index" do
     it "returns http success" do
@@ -16,25 +19,78 @@ RSpec.describe PlansController, type: :controller do
     end
   end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
 
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
 
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "instantiates @plan" do
+      get :new
+      expect(assigns(:plan)).not_to be_nil
+    end
+  end
 
+  describe "POST create" do
+    it "increases the number of Plans by 1" do
+      expect{ post :create, 
+                    params: { 
+                      plan: { 
+                        plan_type: RandomData.random_sentence,
+                        start: RandomData.random_date,
+                        length: RandomData.random_decimal,
+                        unit: RandomData.random_word
+                      }
+                    }
+                  }.to change(Plan,:count).by(1)
+    end
+
+    it "assigns the new plan to @plan" do
+      post :create, 
+            params: {
+              plan: {
+                plan_type: RandomData.random_sentence,
+                start: RandomData.random_date,
+                length: RandomData.random_decimal,
+                unit: RandomData.random_word
+              }
+            }
+      expect(assigns(:plan)).to eq Plan.last
+    end
+
+    it "redirects to the new plan" do
+      post :create,
+            params: {
+              plan: {
+                plan_type: RandomData.random_sentence,
+                start: RandomData.random_date,
+                length: RandomData.random_decimal,
+                unit: RandomData.random_word
+              }
+            }
+      expect(response).to redirect_to Plan.last
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show, params: {id: my_plan.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: {id: my_plan.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_plan to @plan" do
+      get :show, params: {id: my_plan.id}
+      expect(assigns(:plan)).to eq(my_plan)
+    end
+  end
 end
