d6676f062ca42cee390e0af6f27f845ca7855793
timschmidtdev <schmidt.tim.j@gmail.com>
2019-10-07 22:48:25 -0400

    add role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_003830) do
+ActiveRecord::Schema.define(version: 2019_10_08_024239) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_003830) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
   end
 
 end
