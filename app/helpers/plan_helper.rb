bbae659884fb7f84ac04ddba20871be72c2d3550
timschmidtdev <schmidt.tim.j@gmail.com>
2019-10-10 03:34:19 -0400

    calculator logic

--- app/helpers/plan_helper.rb
+++ app/helpers/plan_helper.rb
@@ -1,2 +1,26 @@
 module PlanHelper
+  
+  def unit_of_measure(unit)
+    unit_map = {
+      1 => "Days",
+      2 => "Weeks",
+      3 => "Months",
+      4 => "Years"
+    }
+    
+    unit_map[unit]
+  end
+
+  def calculate_plan(plan)
+    unit = plan.unit.to_i
+    if unit == 1
+      plan.start + plan.length
+    elsif unit == 2
+      plan.start.weeks_since(plan.length)
+    elsif unit == 3
+      plan.start.months_since(plan.length)
+    else
+      plan.start.years_since(plan.length)
+    end
+  end
 end
